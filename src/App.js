74fa3dd08b14df02903d0d5d65e132a5b2371e3b
shuvendu <108450702+Sagittarius17@users.noreply.github.com>
2023-03-24 02:07:23 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Footer } from './components'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Footer />
     </div>
   );
 }
+// const cursor = document.querySelector('#cursor');
 
-export default App;
+// const moveCursor = (e)=> {
+//   const mouseY = e.clientY;
+//   const mouseX = e.clientX;
+   
+//   cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+// }
+
+// window.addEventListener('mousemove', moveCursor)
+export default App
