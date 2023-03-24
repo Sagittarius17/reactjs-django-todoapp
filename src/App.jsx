a6422501d4de6a0273109ce3039dd52df899c7fa
shuvendu <108450702+Sagittarius17@users.noreply.github.com>
2023-03-24 20:25:42 +0530

    going good

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Navbar, Welcome, Footer, Transactions, Services } from "./components";
 
+// import { BG } from './assets/background.png'
 
 const App = () => {
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-welcome">
         <Navbar />
+        {/* <img src={BG} alt="bg" /> */}
         <Welcome />
       </div>
       <Services />
