dd893946594aa524ec822c34f42a8b955f800640
Shuvendu Singha <108450702+Sagittarius17@users.noreply.github.com>
2023-04-11 17:50:35 +0000

    ...

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import { Navbar, Welcome, Footer, Services } from "./components";
 
-// import { BG } from './assets/background.png'
 
 const App = () => {
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-welcome">
         <Navbar />
-        {/* <img src={BG} alt="bg" /> */}
         <Welcome />
       </div>
       <Services />
-      <Transactions />
       <Footer />
     </div>
   );
